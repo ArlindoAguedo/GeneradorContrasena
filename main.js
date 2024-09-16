403e0935ce28a6c1bf0fdd929a8aa8ba62619327
ArlindoAguedo <aaguedot@ucvvirtual.edu.pe>
2024-09-16 18:52:38 -0500

    third commit

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-let cantidad = 6;
+let cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let contrasena = document.getElementById('contrasena');
 
-let texto = "texto";
-console.log(typeof texto);
+const cadenaCaracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-if (cantidad < 6){
-    
+function generar(){
+
+    let numeroDigitado = parseInt(cantidad.value);
+
+    if(numeroDigitado < 8){
+        alert("La cantidad de caracteres tiene que ser mayor que 8");
+    }
+
+    let password = '';
+    for(let i = 0; i < numeroDigitado; i++ ){
+
+        let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+        password += caracterAleatorio; 
+    }
+
+    contrasena.value = password;
 }
-
-
-
-
-
-
